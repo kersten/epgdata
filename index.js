e9e907506cf6eb890018ec466e8c58e9a93f7dd8
Kersten Burkhardt <kerstenk@gmail.com>
2012-09-24 23:09:58 +0200

    tests

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: kerstenburkhardt
- * Date: 24.09.12
- * Time: 19:28
- * To change this template use File | Settings | File Templates.
- */
+module.exports = require("./lib/epgdata");
---
+++ test.js
@@ -0,0 +1,4 @@
+var EpgData = require("./index"),
+    epgdata = new EpgData();
+
+epgdata.unpack("./20120924_20120924_de_qy.zip");
