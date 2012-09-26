5edd97151e507ed27d351593bee2eddb79c9797c
Kersten Burkhardt <kerstenk@gmail.com>
2012-09-26 17:01:21 +0200

    downloader and import

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var EpgData = require("./index"),
-    epgdata = new EpgData();
+    epgdata = new EpgData("UQZy8l7R5wX2098bc2vV16K244h3BBX20Ef0UXpW");
 
-epgdata.unpack("./20120924_20120924_de_qy.zip");
+//epgdata.unpack("./20120924_20120924_de_qy.zip");
